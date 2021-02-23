d3ab40eef96ed190b51cf5ddc57060e4ce0f927d
Bec <70154628+BecWy@users.noreply.github.com>
2021-02-23 19:01:51 +0000

    Added search-bar toggle functionality

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,19 +1,24 @@
 const searchToggle = () => {
 
+    const searchBar = document.querySelector("#search-bar");
     const searchButton = document.querySelector("#search-button");
-
-    const searchInput = document.querySelector("#search-bar input[type=text]");
-    const supportButton = document.querySelector("#support");
-    const contactButton = document.querySelector("#contact");
-
-
+    const searchInput = document.querySelector("#search-bar > input[type=text]"); //#search-bar input[type=text]
+    const supportButton = document.querySelector("#support-button");
+    const contactButton = document.querySelector("#contact-button");
 
     searchButton.addEventListener('click', () => {
-        searchInput.classList.toggle("toggle-hide"); //this toggle is working, the class is being added. But the button isn't being displayed.
-        supportButton.classList.toggle("toggle-show"); //this toggle is working, the class is being added. But the button isn't being displayed.
-        contactButton.classList.toggle("toggle-show"); //this toggle is working, the class is being added. But the button isn't being displayed.
+        searchInput.classList.toggle("toggle-show"); //this toggle is working, the class is being added. But the button isn't being displayed.
+        supportButton.classList.toggle("toggle-hide"); //this toggle is working, the class is being added. But the button isn't being displayed.
+        contactButton.classList.toggle("toggle-hide"); //this toggle is working, the class is being added. But the button isn't being displayed.
+        searchBar.classList.toggle("toggle-search-width"); //expands the width of the search container
+    
+    // if (searchInput.classList.contains(".toggle-show")) {
+    //     searchInput.style.display = "inline-block";
+    // } else {
+    //     searchInput.style.display = "none";
+    // }
+    
     });
 }
-
 
 searchToggle();
