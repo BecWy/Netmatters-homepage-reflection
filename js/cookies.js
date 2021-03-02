0eed3368a282fcb753e4c7bd087c52280f794df8
Bec <70154628+BecWy@users.noreply.github.com>
2021-03-02 14:54:45 +0000

    Added burger menu mobile styles

--- js/cookies.js
+++ js/cookies.js
@@ -3,6 +3,7 @@ const myStorage = window.localStorage;
 const cookies = document.querySelector(".cookies");
 const cookiesButton = document.querySelector("#cookies-button");
 const cookiesOverlay = document.querySelector(".cookies-open-overlay");
+const cookiesOuter = document.querySelector(".cookies-outer-container");
 const body = document.querySelector("body");
 
 document.addEventListener("DOMContentLoaded", ()=> {
@@ -12,11 +13,11 @@ document.addEventListener("DOMContentLoaded", ()=> {
     //Checks if cookies are saved. Decides whether to display the cookies popup or not.
     if (cookiesSaved === 'yes') {
         cookies.style.display = "none";
-        cookiesOverlay.style.display = "none";
+        cookiesOuter.style.display = "none"; //was overlay
         console.log("cookies already accepted"); //for testing purposes
     } else {
         cookies.style.display = "block";
-        cookiesOverlay.style.display = "block";
+        cookiesOuter.style.display = "block"; //was overlay
         console.log("user needs to accept cookies"); //for testing purposes
         body.style.overflowY = "hidden";
     }
@@ -27,6 +28,6 @@ cookiesButton.addEventListener('click', () => {
     myStorage.setItem('cookiesAccepted', 'yes')
     console.log('cookies accepted'); //for testing purposes
     cookies.style.display = "none";
-    cookiesOverlay.style.display = "none";
+    cookiesOuter.style.display = "none"; //was overlay
     body.style.overflowY = "scroll";
 });
