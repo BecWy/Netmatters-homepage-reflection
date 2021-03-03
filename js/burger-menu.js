1742962859a3acbc99e2b3160d79764881c18bdb
Bec <70154628+BecWy@users.noreply.github.com>
2021-03-03 12:55:05 +0000

    Added burger menu styles

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -4,13 +4,14 @@
 const burgerMenu = () => {
 
     const menuButton = document.querySelector("#menu");
+    const header = document.querySelector("header");
     const burgerMenu = document.querySelector(".burger-menu");
     const burgerMenuCont = document.querySelector(".burger-menu-container");
-    const pageContent = document.querySelector("body");
+    const body = document.querySelector("body");
     const main = document.querySelector("main");
     const menuOverlay = document.querySelector(".menu-open-overlay");
-    const nav = document.querySelector("nav");
-    const nav2 = document.querySelector(".nav-2");
+    //const nav = document.querySelector("nav");
+    //const nav2 = document.querySelector(".nav-2");
     const footer = document.querySelector("footer");
 
     menuButton.addEventListener('click', () => {
@@ -25,30 +26,25 @@ const burgerMenu = () => {
 
         /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
         function openNav() {
-            //pageContent.style.width = "100%"; //"calc(100vw - (100vw - 100%))";//"100vw";
-            pageContent.style.overflowY = "hidden";
+            body.style.overflowY = "hidden";
             burgerMenuCont.style.overflowY = "scroll"; //adds menu scroll functionality
-            burgerMenu.style.display = "block";
-            main.style.paddingRight = "calc(100vw - 100%)"; //keeps adjusting by the width of the scrollbar, added this to try and compensate    
-            menuOverlay.style.display = "block";
+            burgerMenu.style.visibility = "visible";
+            burgerMenu.style.transition = "all 1s ease-out 0s";  
             menuOverlay.style.backgroundColor = "rgba(0,0,0, 0.4)";
-            //"property duration timing-function delay|initial|inherit"
-            nav.style.transition = "all 1s ease-out";
-            nav2.style.transition = "all 1s ease-out";
+            menuOverlay.style.zIndex = "800";
+            header.style.transition = "all 1s ease-out";
             main.style.transition = "all 1s ease-out";
             footer.style.transition = "all 1s ease-out";
             menuOverlay.style.transition = "all 1s ease-out";
 
 
             if(window.matchMedia('(min-width: 993px)').matches) { //wide screens
-                nav.style.transform = "translate(-350px, 0)";
-                nav2.style.transform = "translate(-350px, 0)";
+                header.style.transform = "translate(-350px, 0)";
                 main.style.transform = "translate(-350px, 0)"; 
                 footer.style.transform = "translate(-350px, 0)";
                 menuOverlay.style.transform = "translate(-350px, 0)";
             } else {  //small screens
-                nav.style.transform = "translate(-270px, 0)";
-                nav2.style.transform = "translate(-270px, 0)";
+                header.style.transform = "translate(-270px, 0)";
                 main.style.transform = "translate(-270px, 0)"; 
                 footer.style.transform = "translate(-270px, 0)";
                 menuOverlay.style.transform = "translate(-270px, 0)";
@@ -57,19 +53,16 @@ const burgerMenu = () => {
         
         /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
         function closeNav() {
-            //pageContent.style.transform = "none";
-            //burgerMenu.style.display = "none";
-            nav.style.transform = "none";
-            nav2.style.transform = "none";
+            header.style.transform = "none";
             main.style.transform = "none";
             footer.style.transform = "none";
             menuOverlay.style.transform = "none";
-            menuOverlay.style.display = "none";
             menuOverlay.style.backgroundColor = "rgba(0,0,0, 0)";
-            pageContent.style.overflowY = "auto";
-            //pageContent.style.width = "100%";
+            menuOverlay.style.zIndex = "0";
+            body.style.overflowY = "auto";
             burgerMenuCont.style.overflowY = "hidden"; //removes menu scroll functionality
-            main.style.paddingRight = "0px"; //keeps adjusting by the width of the scrollbar, added this to try and compensate
+            burgerMenu.style.visibility = "hidden";
+            burgerMenu.style.transition = "all 1s ease-out 3s"; //doesn't disappear until covered by the main content again
         }
 
 
@@ -85,29 +78,6 @@ const burgerMenu = () => {
 
 burgerMenu();
 
-
-
-
-
-//CSS///////////////////////////////////////////////////////////////////////////////////////////
-//looks like i need to set the width of the menu to 0 inititally
-//   width: 0; /* 0 width - change this with JavaScript */
-// overflow-x: hidden; /* Disable horizontal scroll */
-// transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
-
-/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
-// #main {
-//     transition: margin-left .5s;
-//     padding: 20px;
-//   }
-
-
-
-/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
-// @media screen and (max-height: 450px) {
-//     .sidenav {padding-top: 15px;}
-//     .sidenav a {font-size: 18px;}
-//   }
 
 
 
