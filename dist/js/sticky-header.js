ef8e06542111864b552dd1b87fccb4da3cd4b2ee
Bec <70154628+BecWy@users.noreply.github.com>
2021-03-12 10:43:52 +0000

    Tidied sticky-header.js

--- dist/js/sticky-header.js
+++ dist/js/sticky-header.js
@@ -45,27 +45,30 @@ var bodyCont = document.querySelector(".body-container");
 var stickyHeader = function stickyHeader() {
   //re-activate when switch back to the app js file after testing
   //const stickyHeader = () => { // this line is for testing only
+  //when the body content is scrolls the sticky function is run
   bodyCont.onscroll = function () {
     sticky();
   };
 
-  var previous = 0;
+  var previous = 0; //this is the previous value of scrolltop. It is used to compare with the new value of scrolltop.
 
   function sticky() {
-    //   if (bodyCont.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //compares the current value of scrolltop with the previous value of scrolltop to determine the scroll direction.
+    //Scrolling down - hide the top nav
     if (bodyCont.scrollTop > 50 && bodyCont.scrollTop > previous) {
       //header.className = "nav-hide";
       header.classList.remove("nav-show");
       header.classList.add("nav-hide");
-    } else {
-      //header.className = "nav-show";
-      header.classList.remove("nav-hide");
-      header.classList.add("nav-show");
-    }
+    } //Scrolling up - display the top nav
+    else {
+        //header.className = "nav-show";
+        header.classList.remove("nav-hide");
+        header.classList.add("nav-show");
+      } //set the current value as the new previous value so that it can be used in the next comparison.
+
 
     previous = bodyCont.scrollTop;
   }
-};
-stickyHeader();
+}; //stickyHeader(); for when testing this as a separate file only
 /******/ })()
 ;
--- js/sticky-header.js
+++ js/sticky-header.js
@@ -6,22 +6,30 @@ const bodyCont = document.querySelector(".body-container");
 
 export const stickyHeader = () => { //re-activate when switch back to the app js file after testing
 //const stickyHeader = () => { // this line is for testing only
+
+//when the body content is scrolls the sticky function is run
   bodyCont.onscroll = function() {sticky()};
-  let previous = 0;
+  let previous = 0; //this is the previous value of scrolltop. It is used to compare with the new value of scrolltop.
 
   function sticky() {
-  //   if (bodyCont.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      if (bodyCont.scrollTop > 50 && bodyCont.scrollTop > previous) {
+  //compares the current value of scrolltop with the previous value of scrolltop to determine the scroll direction.
+      
+    //Scrolling down - hide the top nav
+    if (bodyCont.scrollTop > 50 && bodyCont.scrollTop > previous) {
       //header.className = "nav-hide";
       header.classList.remove("nav-show");
       header.classList.add("nav-hide");
-    } else {
+    } 
+      //Scrolling up - display the top nav
+      else {
       //header.className = "nav-show";
       header.classList.remove("nav-hide");
       header.classList.add("nav-show");
     }
-    previous = bodyCont.scrollTop;
+
+      //set the current value as the new previous value so that it can be used in the next comparison.
+      previous = bodyCont.scrollTop;
   }
 }
 
-stickyHeader();
+//stickyHeader(); for when testing this as a separate file only
