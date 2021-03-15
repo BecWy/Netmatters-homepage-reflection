604d51e3768afcf7718252f2f28d1d1c8e2882b0
Bec <70154628+BecWy@users.noreply.github.com>
2021-03-15 10:29:08 +0000

    Added sticky header IE fixes

--- dist/js/burger-menu.js
+++ dist/js/burger-menu.js
@@ -47,56 +47,71 @@ var header = document.querySelector("header");
 var bodyContWidth = document.querySelector(".body-container").clientWidth; //this is the width of the body container. It is used as a comparison for the header, to make sure that the header width is always the same as the body width. Position fixed in IE will make the header cover the scroll bar otherwise
 //for IE purposes. Returns the value of the header's css position - sticky or fixed
 
-var headerPosition = window.getComputedStyle(header).getPropertyValue('position').toLowerCase(); //console.log(`the header position is ${headerPosition}`); for testing
+var headerPosition = window.getComputedStyle(header).getPropertyValue('position').toLowerCase(); //console.log(`the header position is ${headerPosition}`); //for testing
 
+var internetExplorer = false;
 var burgerMenuJS = function burgerMenuJS() {
   //re-activate when switch back to the app js file after testing
   //const burgerMenuJS = () => { // this line is for testing only
+  //determine if the browser supports position: sticky
+  if (headerPosition === "sticky") {
+    internetExplorer = false;
+    console.log("the browser is modern and supports sticky");
+  } else {
+    internetExplorer = true;
+    console.log("the browser is old and does not support sticky");
+  } //on page load the side menu is closed
+
+
   document.addEventListener('DOMContentLoaded', function () {
     closeNav();
-    bodyContWidth = document.querySelector(".body-container").clientWidth;
-    header.style.width = "".concat(bodyContWidth, "px"); //makes sure the header is the correct width if set to position:fixed (for IE sticky header settings)
   }); //When the burger menu icon is clicked the side menu is revealed
 
   menuButton.addEventListener('click', function () {
     openNav();
-    bodyContWidth = document.querySelector(".body-container").clientWidth;
-    header.style.width = "".concat(bodyContWidth, "px"); //makes sure the header is the correct width if set to position:fixed (for IE sticky header settings)
   }); //when the overlay over the main page content is clicked the side menu is hidden.
 
   menuOverlay.addEventListener('click', function () {
     closeNav();
-    bodyContWidth = document.querySelector(".body-container").clientWidth;
-    header.style.width = "".concat(bodyContWidth, "px"); //makes sure the header is the correct width if set to position:fixed (for IE sticky header settings)
   }); //is the menu open
 
   var menuOpen = false; //When the page is resized, this updates the distance the page content translates to the left
 
   window.addEventListener('resize', function () {
-    bodyContWidth = document.querySelector(".body-container").clientWidth; //get the value each time
+    if (internetExplorer === true) {
+      bodyContWidth = document.querySelector(".body-container").clientWidth; //get the value each time
 
-    header.style.width = "".concat(bodyContWidth, "px"); //makes sure the header is the correct width if set to position:fixed (for IE sticky header settings)
-    //for wide screens with the menu open translate the page content by the correct distance
+      header.style.width = "".concat(bodyContWidth, "px"); //makes sure the header is the correct width if set to position:fixed (for IE sticky header settings)
+    } //for wide screens with the menu open translate the page content by the correct distance
+
 
     if (window.matchMedia('(min-width: 993px)').matches && menuOpen === true) {
       bodyCont.style.transform = "translateX(-350px)";
       menuOverlay.style.transform = "translateX(-350px)";
       bodyCont.style.transition = "none";
       menuOverlay.style.transition = "none";
-      header.style.transition = "none"; //header.style.transform = "none"
-      //for small screens with the menu open translate the page content by the correct distance
+      header.style.transition = "none"; //only applies to IE - makes sure there's no transition
+
+      if (internetExplorer === true) {
+        header.style.transform = "translateX(-350px)";
+      } //for small screens with the menu open translate the page content by the correct distance
+
     } else if (menuOpen === true) {
       //small screens
       bodyCont.style.transform = "translateX(-270px)";
       menuOverlay.style.transform = "translateX(-270px)";
       bodyCont.style.transition = "none";
       menuOverlay.style.transition = "none";
-      header.style.transition = "none"; //header.style.transform = "none"
-      //when the menu is closed and resized, this avoids part of the menu displaying unintentionally due to the transition time
+      header.style.transition = "none"; //only applies to IE - makes sure there's no transition
+
+      if (internetExplorer === true) {
+        header.style.transform = "translateX(-270px)";
+      } //when the menu is closed and resized, this avoids part of the menu displaying unintentionally due to the transition time
+
     } else {
       bodyCont.style.transition = "none";
       menuOverlay.style.transition = "none";
-      header.style.transition = "none"; //header.style.transform = "none"
+      header.style.transition = "none"; //only applies to IE - makes sure there's no transition
     }
   }); //pushes the page content to the left, revealing the sidebar below
 
@@ -108,14 +123,17 @@ var burgerMenuJS = function burgerMenuJS() {
 
     bodyCont.style.transition = "all .5s ease-out";
     menuOverlay.style.transition = "all .5s ease-out";
-    header.style.transition = "all .5s ease-out"; //header.style.paddingTop = bodyCont.scrollTop;
+
+    if (internetExplorer === true) {
+      header.style.transition = "all .5s ease-out"; //header.style.paddingTop = bodyCont.scrollTop;
+    }
 
     if (window.matchMedia('(min-width: 993px)').matches) {
       //wide screens
       bodyCont.style.transform = "translateX(-350px)";
       menuOverlay.style.transform = "translateX(-350px)"; //if the browser is IE and therefore the position setting is fixed instead of sticky
 
-      if (headerPosition !== "sticky") {
+      if (internetExplorer === true) {
         header.style.transform = "translateX(-350px)";
       }
     } else {
@@ -124,10 +142,10 @@ var burgerMenuJS = function burgerMenuJS() {
       menuOverlay.style.transform = "translateX(-270px)";
       header.style.transform = "translateX(-270px)"; //if the browser is IE and therefore the position setting is fixed instead of sticky
 
-      if (headerPosition !== "sticky") {
+      if (internetExplorer === true) {
         //console.log(`header position is ${headerPosition}`) testing
         header.style.transform = "translateX(-270px)";
-      } else if (headerPosition === "sticky") {
+      } else {
         header.style.transform = "none";
       }
     }
@@ -139,13 +157,21 @@ var burgerMenuJS = function burgerMenuJS() {
   var closeNav = function closeNav() {
     bodyCont.style.transform = "none"; //IMPORTANT - has to be set to none otherwise it interferes with the position:fixed needed for the sticky header. It's a weird quirk - if the element you want to position has any ancestor with a transform property it won't position correctly.
 
-    header.style.transform = "none"; //if the translate was applied on open nav (aka in IE) then it is now removed.
-
     menuOverlay.style.transform = "translateX(0px)";
     menuOverlay.style.backgroundColor = "rgba(0,0,0, 0)";
     menuOverlay.style.zIndex = "0";
     bodyCont.style.transition = "all .5s ease-out";
     menuOverlay.style.transition = "all .5s ease-out";
+
+    if (internetExplorer === true) {
+      header.style.transition = "all .5s ease-out";
+      header.style.transform = "none"; //if the translate was applied on open nav (aka in IE) then it is now removed.
+
+      bodyContWidth = document.querySelector(".body-container").clientWidth; //get the value each time
+
+      header.style.width = "".concat(bodyContWidth, "px"); //makes sure the header is the correct width if set to position:fixed (for IE sticky header settings)
+    }
+
     setTimeout(function () {
       burgerMenuCont.scrollTop = 0;
     }, 600);
